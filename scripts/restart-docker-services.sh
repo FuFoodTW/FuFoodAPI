024c6dd4adce7a7218fe48e21f078644bb3e75ca
Karol Moroz <karol@moroz.dev>
2025-11-23 12:04:38 +0800

    docker-compose => docker compose

--- scripts/restart-docker-services.sh
+++ scripts/restart-docker-services.sh
@@ -9,6 +9,6 @@ AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-ap-northeast-1}"
 # /usr/local/server/backup.sh
 
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-docker-compose -f $DOCKER_COMPOSE_PATH pull
-docker-compose -f $DOCKER_COMPOSE_PATH up -d
+docker compose -f $DOCKER_COMPOSE_PATH pull
+docker compose -f $DOCKER_COMPOSE_PATH up -d
 

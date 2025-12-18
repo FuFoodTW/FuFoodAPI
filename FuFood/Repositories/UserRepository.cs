5aba2e0a0ea03db27b0522bb800d6a54d72e8172
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-12-18 19:28:31 +0800

    Set IsDefault flag on default refrigerator

--- FuFood/Repositories/UserRepository.cs
+++ FuFood/Repositories/UserRepository.cs
@@ -27,7 +27,8 @@ public class UserRepository(AppDbContext db)
         var defaultRefrigerator = new Refrigerator
         {
             Name = "我的冰箱",
-            CreatedById = user.Id
+            CreatedById = user.Id,
+            IsDefault = true
         };
 
         db.Refrigerators.Add(defaultRefrigerator);

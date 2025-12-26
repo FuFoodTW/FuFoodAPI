f84319e6a14b025b41683dddc171bdff2a9216d6
Karol Moroz <karol@moroz.dev>
2025-12-27 02:51:35 +0800

    Fix seeds

--- FuFood/Data/Seeds.cs
+++ FuFood/Data/Seeds.cs
@@ -120,7 +120,7 @@ public static class Seeds
                 },
                 new InventoryTransactionItem
                 {
-                    Id = milkItemId,
+                    Id = Guid.Parse("019b5618-01a3-77be-97fd-0b99cc24004f"),
                     InventoryTransactionId = storingTransactionId,
                     ProductId = sushiId,
                     Quantity = 1,

8b45df333f239efa9238157a4934624209c6c013
Karol Moroz <karol@moroz.dev>
2025-12-28 19:52:36 +0800

    Set up tests with HTTP access

---
+++ FuFood.Tests/GlobalTestFixture.cs
@@ -0,0 +1,35 @@
+using FuFood.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FuFood.Tests;
+
+public class GlobalTestFixture : IAsyncLifetime
+{
+    private WebApplicationFactory<Program>? _factory;
+
+    public WebApplicationFactory<Program> Factory =>
+        _factory ?? throw new InvalidOperationException("Factory is not initialized");
+
+    public AsyncServiceScope AsyncScope => Factory.Services.CreateAsyncScope();
+
+    public async Task InitializeAsync()
+    {
+        _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => builder.UseEnvironment("Test"));
+        await MigrateDb();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    private async Task MigrateDb()
+    {
+        await using var scope = AsyncScope;
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
+    }
+}

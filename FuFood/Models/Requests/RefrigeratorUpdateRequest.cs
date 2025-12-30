4882fc3c55920bb3a481894280718add77a82e51
Karol Moroz <karol@moroz.dev>
2025-12-31 00:52:17 +0800

    Remove Colours from RefrigeratorUpdateRequest

--- FuFood/Models/Requests/RefrigeratorUpdateRequest.cs
+++ FuFood/Models/Requests/RefrigeratorUpdateRequest.cs
@@ -5,5 +5,4 @@ namespace FuFood.Models.Requests;
 public class RefrigeratorUpdateRequest
 {
     [Required, StringLength(100)] public string Name { get; set; } = null!;
-    [Required, StringLength(30)] public string? Colour { get; set; }
 }
